c2f5ba9d797e6778e099180a23d72b2479e95e1f
a22918097 <a22918097@gmail.com>
2015-08-19 18:46:57 +0800

    tired

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,8 +9,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-
 }
 
 MainWindow::~MainWindow()
@@ -23,19 +21,40 @@ void MainWindow::on_pushButton_load_clicked()
     QFile file("todo.txt");
     if(file.open(QIODevice::ReadOnly))
     {
+
         QString line;
         QTextStream in(&file);
         while(!in.atEnd())
         {
             line=in.readLine();
+            QStringList strList = line.split(",");
+            //qDebug() << strList[0] <<strList[1] <<strList[2];
+            QDate date;
+            date = QDate::fromString(strList[0],"yyyy/MM/dd");
+            //qDebug() << date;
+            QTime time;
+            time = QTime::fromString(strList[1],"HH:mm");
+            QDate nowdate;
+            nowdate=nowdate.currentDate();
+            QTime nowtime;
+            nowtime=nowtime.currentTime();
             ui->textBrowser_2->append(line);
+            if(date==nowdate&&time.addSecs(-30*60)<nowtime&&time>nowtime)
+            {
+                //ui->textBrowser_2->clear();
+                ui->textBrowser_2->setTextColor(2000);
+                ui->textBrowser_2->append("It's time to "+  strList[2]);
+                qDebug() << "It's time to " << strList[2];
+                ui->textBrowser_2->setTextColor(1);
+
+
+        }
         }
     }
     else{
     qDebug() << "Fail";
   }
 }
-
 
 void MainWindow::on_calendarWidget_clicked(const QDate &date)
 {
@@ -49,14 +68,25 @@ void MainWindow::on_calendarWidget_clicked(const QDate &date)
 void MainWindow::on_pushButton_OK_clicked()
 {
     ui->textBrowser_2->append(showdate);
-    ui->textBrowser_2->insertPlainText("   "+ui->timeEdit->text());
-    ui->textBrowser_2->insertPlainText("   "+ui->textEdit->toPlainText());
+    ui->textBrowser_2->insertPlainText(","+ui->timeEdit->text());
+    ui->textBrowser_2->insertPlainText(","+ui->textEdit->toPlainText());
     ui->textEdit->clear();
 
     ui->calendarWidget->setEnabled(1);
     ui->timeEdit->setEnabled(0);
     ui->textEdit->setEnabled(0);
     ui->pushButton_save->setEnabled(1);
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -73,4 +103,29 @@ void MainWindow::on_pushButton_save_clicked()
         QTextStream out(&file);
         out << ui->textBrowser_2->toPlainText();
     }
+}
+//QString str = "2013-10-15,14:00,sleep";
+
+//    QStringList strList = str.split(",");
+
+//    qDebug() << strList[0] <<strList[1]<<strList[2];
+
+//    QDate date;
+//    date = QDate::fromString(strList[0],"yyyy/MM/dd");
+//    qDebug() << date;
+
+//    QTime time;
+//    time = QTime::fromString(strList[1],"HH:mm");
+//    qDebug() << time;
+//
+
+
+
+
+
+
+
+void MainWindow::on_textEdit_cursorPositionChanged()
+{
+    ui->pushButton_OK->setEnabled(1);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     QString showdate;
+    QString str;
     ~MainWindow();
 
 private slots:
@@ -28,6 +29,8 @@ private slots:
     void on_timeEdit_timeChanged(const QTime &time);
 
     void on_pushButton_save_clicked();
+
+    void on_textEdit_cursorPositionChanged();
 
 private:
     Ui::MainWindow *ui;
